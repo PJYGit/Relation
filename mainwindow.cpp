bd577478e5e5385631ef8af4f6475ea34529c7f5
Machenike <870096160@qq.com>
2019-04-26 09:54:23 +0800

    Almost finished. Ugly UI but it's ok.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "relation.h"
+#include <QStack>
+#include <QPushButton>
+#include <QFileDialog>
+
+int count_level = 1;
+int hori_count = 1;
+QStack<int> stack;
+int added[20];
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    Pix = QPixmap(width(), height());
+    Pix.fill(Qt::gray);
+
+    for (int i=0;i<20;i++) added[i] = -1;
 }
 
 MainWindow::~MainWindow()
@@ -17,9 +30,12 @@ MainWindow::~MainWindow()
 void MainWindow::on_EquivalenceClosure_clicked()
 {
     ui->Outputs->setPlainText("");
+
+    QString filename = QFileDialog::getOpenFileName(this, "Matrix choosing", QDir::currentPath());
+
     Relation r;
     Relation rout;
-    r=Relation("F:/Qt codes/Relation/test1.txt");
+    r=Relation(filename.toLatin1().data());
     rout = r.EquivalenceClosure();
 
     int dimension = rout.GetDimension();
@@ -46,8 +62,11 @@ void MainWindow::on_EquivalenceClosure_clicked()
 void MainWindow::on_EquivalenceClasses_clicked()
 {
     ui->Outputs->setPlainText("");
+    QString filename = QFileDialog::getOpenFileName(this, "Matrix choosing", QDir::currentPath());
+
     Relation r;
-    r=Relation("F:/Qt codes/Relation/test1.txt");
+    Relation rr;
+    r=Relation(filename.toLatin1().data());
 
     int *EClass = new int[r.GetDimension()];
     r.EquivalenceClosure().EquiClasses(EClass);
@@ -104,19 +123,27 @@ void MainWindow::on_EquivalenceClasses_clicked()
             break;
         }
     }
+
+    rr = r.EquivalenceClosure();
+    if (rr.GetatPosition(8,12) == 1){
+        ui->Outputs->append("No 9&13 in one module!");
+    }
+
 }
 
 void MainWindow::on_Topology_clicked()
 {
     ui->Outputs->setPlainText("");
-    Relation r("F:/Qt codes/Relation/test5.txt");
+    QString filename = QFileDialog::getOpenFileName(this, "Matrix choosing", QDir::currentPath());
+
+    Relation r(filename.toLatin1().data());
     if (r.IsCycle()){
         ui->Outputs->setPlainText("There is a cycle! No Topology Order!");
     }
     else {
         int *topo = new int[r.GetDimension()];
         r.Topology(topo);
-        ui->Outputs->setPlainText("The topology order is:\n");
+        ui->Outputs->setPlainText("One topology order is:\n");
         for (int i = 0; i < r.GetDimension(); i++) {
             QString last = ui->Outputs->toPlainText();
             QString temp = QString::number(topo[i] + 1);
@@ -129,7 +156,8 @@ void MainWindow::on_Topology_clicked()
 void MainWindow::on_Hasse_clicked()
 {
     ui->Outputs->setPlainText("");
-    Relation r("F:/Qt codes/Relation/test6.txt");
+    QString filename = QFileDialog::getOpenFileName(this, "Matrix choosing", QDir::currentPath());
+    Relation r(filename.toLatin1().data());
     Relation rpartial;
     Relation rout;
     rpartial = r.PartialorderClosure();
@@ -155,17 +183,127 @@ void MainWindow::on_Hasse_clicked()
             }
             ui->Outputs->append("");
         }
+        GenerateButton(temp,dimension);
+
+        for (int t=0;t<dimension;t++){
+            AddNum(t);
+        }
+
+        int *emm = new int[dimension * dimension];
+        rout.GetMatrix(emm);
+        for (int m=0;m<dimension;m++){
+            for (int n=0;n<dimension;n++){
+                if (emm[m*dimension+n] == 1){
+                    DrawLines(m, n, true);
+                    this->repaint();
+                }
+            }
+        }
+
     }
     else {
         ui->Outputs->setPlainText("NO HASSE!");
     }
 }
 
+void MainWindow::paintEvent(QPaintEvent *event) {
+    QPainter Painter(this);
+    Painter.drawPixmap(0, 0, width(), height(), Pix);
+}
+
+void MainWindow::my_paintLine(int x1, int y1, int x2, int y2, bool color){
+
+    QPainter Painter(&Pix);
+    if(color){
+        Painter.setPen(QPen(Qt::black, 2));
+    }
+    else {
+        Painter.setPen(QPen(Qt::red, 2));
+    }
+
+    Painter.drawLine(x1, y1, x2, y2);
+}
+
+void MainWindow::GenerateButton(int *hasse, int dimension){
+    bool check;
+    int number;
+
+    while (count_level<=dimension) {  
+        for (int column = 0; column < dimension; column++)
+        {
+            check = true;
+            for (int row = 0; row < dimension; row++)
+            {
+                if (hasse[row*dimension+column] == 1) {
+                    check = false;
+                }
+            }
+
+            if (check){
+                check = false;
+                if (added[column] != 1)
+                    stack.push(column);
+                added[column] = 1;
+            }
+        }
 
 
+        number = stack.size();
+        while (!stack.isEmpty()){
+            int temp = stack.pop();
+            for (int p=0;p<dimension;p++){
+                hasse[temp*dimension+p] = 0;
+            }
 
+            QPushButton *but = new QPushButton(ui->centralWidget);
+            but->setFixedSize(12,12);
+            but->setObjectName(QString::number(temp));
+            //but->setText(QString::number(temp+1));
+            but->setStyleSheet("background-color:rgb(0,255,0);border:1px solid green;border-radius:5px");
+            but->setGeometry(GetPos(number,hori_count, count_level));
+            but->show();
+            hori_count++;
+        }
+        hori_count = 1;
+        count_level++;
+    }
 
+}
 
+QRect MainWindow::GetPos(int f, int hor, int s){
+    int part = 520 / (f+1);
+
+    int x = part * hor - 15;
+
+    int y = 504 - 60 * (s-1);
+    return QRect(x, y, 30, 30);
+}
+
+void MainWindow::DrawLines(int fnode, int snode, bool mode){
+    QString temp1 = QString::number(fnode);
+    QString temp2 = QString::number(snode);
+    QPushButton *tempBut1 = ui->centralWidget->findChild<QPushButton*>(temp1);
+    QPushButton *tempBut2 = ui->centralWidget->findChild<QPushButton*>(temp2);
+    int a = tempBut1->x()+3;
+    int b = tempBut1->y()+17;
+    int c = tempBut2->x()+3;
+    int d = tempBut2->y()+17;
+
+    my_paintLine(a,b,c,d,mode);
+}
+
+void MainWindow::AddNum(int node){
+    QString temp = QString::number(node);
+    QString num_name = QString::number(node+1);
+    QPushButton *tempBut = ui->centralWidget->findChild<QPushButton*>(temp);
+    int a = tempBut->x()-10;
+    int b = tempBut->y();
+    QLabel *node_num = new QLabel(ui->centralWidget);
+    node_num->setText(num_name);
+    node_num->setObjectName(temp);
+    node_num->setGeometry(a, b, 15, 15);
+    node_num->show();
+}
 
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QColor>
+#include <QPixmap>
+#include <QPainter>
 
 namespace Ui {
 class MainWindow;
@@ -15,6 +18,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void paintEvent(QPaintEvent *event);
+    void my_paintLine(int x1, int y1, int x2, int y2, bool color);
+
+    void GenerateButton(int *hasse, int dimension);
+    QRect GetPos(int f, int hor, int s);
+
+    void DrawLines(int fnode, int snode, bool mode);
+    void AddNum(int node);
+
 private slots:
     void on_EquivalenceClosure_clicked();
 
@@ -26,6 +38,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    QPixmap Pix;
 };
 
 #endif // MAINWINDOW_H
